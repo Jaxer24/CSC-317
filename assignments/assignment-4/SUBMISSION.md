756150af443b0be8fb3b2baddf9ed0364c3f6901
Jaxer24 <jacobarciniega24@gmail.com>
2025-04-16 23:51:30 -0700

    uploading final portfolio with calculator, assignment 4

---
+++ assignments/assignment-4/SUBMISSION.md
@@ -0,0 +1,38 @@
+Name: Jacob Arciniega-Bueno
+Student ID: 924000204
+GitHub Username: Jaxer24
+Assignment Number: 4
+
+Links to:
+-GitHub repository: https://github.com/Jaxer24/CSC-317/tree/main/assignments/assignment-4
+-Live GitHub Pages URL: https://jaxer24.github.io/CSC-317/
+
+Brief description of your implementation (100-200 words):
+    For this assignmment I had to practice extensive problem solving as I struggled with part 1, making the calculator, 
+    simply because I chose to dive head into the assignment without properly planing or prepairing my layout or general 
+    strategy. This lead to me discovering many errors that I had to fix along the way painstakingly, and this has served 
+    as a learning expereince for me. I tried to condense my code and reuse the same functions for both the keyboard and 
+    click events to keep my code efficient, however my javascript still ended up being long, I do feel like I could condense
+    the code further but it would not drastically improve the end product. In the future I will make sure to set up the css 
+    layout before I start my javascript as trying to fix it after the fact got complex fast. 
+
+
+Any challenges you faced and how you overcame them:
+    I found the CSS portion of the assignment more 
+    difficult than the javascript portion because I am very used to javascript, however struggling to get the CSS working 
+    right helped me learn a lot of the fundemental mechanics behind the language. At first I tried to handle the dynamic
+    sizing of the calculator using relative units such as %, fr, and vw/vh for each element in the calculator. This was 
+    extremely problematic as resizing the page caused many issues, so I ended up using a series of 5 media queries that 
+    statically change the calculator size depending on screen width, while not as clean or dynamic as I wanted it to look
+    I think it works well. I also had a huge problem with displaying the % symbol in my calculator so I had to create a work
+    around that turned numbers into their decimal form instead of showing them in percent form, while this mathematically 
+    works the same I do wish I could have fixed this to make it more accurate to the real app.
+    
+Any additional features or improvements you added beyond the requirements:
+    The only extra bit I added that is not in the requirments is a scrollbar that allows users to handle larger numbers since
+    they won't be hindered by the display size, I think this feature makes the calculator act like a ti-84 that also has the 
+    scrolling, I simply thought it was a better way of implementing the display rather than having numbers cut off or shrink 
+    too small.
+    
+Acknowledgment of any resources, tutorials, or code snippets you used:
+    I used the secureEval function provided in discord. 
---
+++ assignments/assignment-4/scripts/calculator.js
@@ -0,0 +1,248 @@
+//initialize global variables
+const opperations = ["+", "-", "*", "/"];
+const buttonSafePattern = /[0-9]/;
+const opSafePattern = /[+*/-]/;
+const safePattern = /^[0-9+*/%.() -]+$/;
+let userInput = false;
+
+//AC logic handler
+function clear(){
+        let text = document.getElementById("display_input").textContent;
+        console.log(text);
+        if(text != "0"){
+                document.getElementById("display_input").textContent = "0";
+        }else{
+                document.getElementById("display_output").textContent = "";
+        }
+}
+
+//sign change logic handler
+function sign() {
+        let textIn = document.getElementById("display_input").textContent;
+        let firstCh = textIn.charAt(0);
+        if(firstCh == "-"){
+                document.getElementById("display_input").textContent = textIn.slice(1,textIn.length);
+        }else{
+                document.getElementById("display_input").textContent = "-"+textIn;
+        }
+}
+
+//peercentage logic handler
+function percent(){
+        let textIn = document.getElementById("display_input").textContent;
+        let textOut = document.getElementById("display_output").textContent;
+        let lastOutCh = textOut.charAt(textOut.length-1);
+        if(lastOutCh == "+"){
+                let a = secureEval(textOut.slice(0, -1)); 
+                document.getElementById("display_input").textContent = a + a * textIn/100;
+                document.getElementById("display_output").textContent = textOut + a * textIn /100;
+        }else if(lastOutCh == "-"){
+                let a = secureEval(textOut.slice(0, -1)); 
+                document.getElementById("display_input").textContent = a - a * textIn/100;
+                document.getElementById("display_output").textContent = textOut + a * textIn /100;
+        }else if(lastOutCh == "*"){
+                let a = secureEval(textOut.slice(0, -1)); 
+                document.getElementById("display_input").textContent = a * (textIn/100);
+                document.getElementById("display_output").textContent = textOut + textIn /100;
+        }else if(lastOutCh == "/"){
+                let a = secureEval(textOut.slice(0, -1)); 
+                document.getElementById("display_input").textContent = a / (textIn/100);
+                document.getElementById("display_output").textContent = textOut + textIn /100;
+        }else{
+                document.getElementById("display_input").textContent = textIn /100;
+                document.getElementById("display_output").textContent = textIn /100;
+        }
+}
+
+//decimal logic handler
+function decimal(){
+        let textIn = document.getElementById("display_input").textContent;
+        let lastInCh = textIn.charAt(textIn.length-1);
+        if(!textIn.includes(".")){
+                document.getElementById("display_input").textContent = textIn+".";
+        }
+}
+
+//equal button logic handler
+function equals(){
+        let textIn = document.getElementById("display_input").textContent;
+        let textOut = document.getElementById("display_output").textContent;
+        let lastOutCh = textOut.charAt(textOut.length-1);
+        let x = "";
+        
+        if(textOut.length==0){
+                return;
+        }else if(textOut.includes("Error") || textIn.includes("Error")){
+                document.getElementById("display_input").textContent = "0";
+                document.getElementById("display_output").textContent = "";
+        }
+        else if(opperations.includes(lastOutCh)){
+                
+                document.getElementById("display_input").textContent = secureEval(textOut+textIn);
+                document.getElementById("display_output").textContent = textOut+textIn;
+        }else {
+                for(let i=textOut.length-1 ; i>0 ; i--){
+                        if(!opperations.includes(textOut.charAt(i))){
+                                x += textOut.charAt(i);
+                        }else{
+                                x += textOut.charAt(i);
+                                x = reverseString(x);
+                                break;
+                        }
+                }
+                        console.log(textOut+textIn);
+                document.getElementById("display_input").textContent = secureEval(textIn+x);
+                document.getElementById("display_output").textContent = textIn+x;
+        }
+        
+}
+
+//operator button logic handler
+function opButton(symbol){
+        let textIn = document.getElementById("display_input").textContent;
+        let textOut = document.getElementById("display_output").textContent;
+        let lastOutCh = textOut.charAt(textOut.length - 1);
+        
+        if(userInput == true){
+                if(opperations.includes(lastOutCh)){
+                        document.getElementById("display_input").textContent = secureEval(textOut+textIn);
+                        document.getElementById("display_output").textContent = textOut+textIn+symbol;
+                }else{
+                        if(textOut == ""){
+                                document.getElementById("display_output").textContent = textIn+symbol;
+                        }else{
+                                document.getElementById("display_output").textContent = textOut+symbol;
+                        }
+                        
+                }
+        }else{
+                if (opperations.includes(lastOutCh)){
+                        document.getElementById("display_output").textContent = textOut.slice(0,-1)+symbol;
+                } else {
+                        document.getElementById("display_output").textContent = secureEval(textOut+textIn)+symbol;
+                } 
+        }
+        userInput = false;
+        
+}
+//operator button click handler
+function opButtonPress(id, symbol){
+        document.getElementById(id).addEventListener("click", function() {
+                opButton(symbol);
+        });
+        
+}
+
+//number button logic handler
+function numButton(symbol){
+        let textIn = document.getElementById("display_input").textContent;
+        let textOut = document.getElementById("display_output").textContent;
+        
+        if(userInput == false || textIn.includes("Error") || textOut.includes("Error")){
+                document.getElementById("display_input").textContent = symbol;
+                if(textIn.includes("Error") || textOut.includes("Error")){
+                document.getElementById("display_output").textContent = "";
+                }
+        } else{
+                let lastOutCh = textOut.charAt(textOut.length-1);
+                if (textIn==0){
+                        document.getElementById("display_input").textContent = symbol;
+                }else{
+                        document.getElementById("display_input").textContent = textIn+symbol;
+                }
+        }
+        userInput = true;
+        
+}
+//number button click handler
+function numButtonPress(id, symbol){
+        document.getElementById(id).addEventListener("click", function() {
+                numButton(symbol);
+        });
+        
+} 
+
+//run all click handlers
+opButtonPress("plus", "+");
+opButtonPress("minus", "-");
+opButtonPress("multiply", "*");
+opButtonPress("divide", "/");
+numButtonPress("zero", "0");
+numButtonPress("one", "1");
+numButtonPress("two", "2");
+numButtonPress("three", "3");
+numButtonPress("four", "4");
+numButtonPress("five", "5");
+numButtonPress("six", "6");
+numButtonPress("seven", "7");
+numButtonPress("eight", "8");
+numButtonPress("nine", "9");
+//special button click handlers
+document.getElementById("equals").addEventListener("click", function() {
+        equals();
+});
+document.getElementById("decimal").addEventListener("click", function() {
+        decimal();
+});
+document.getElementById("percent").addEventListener("click", function() {
+        percent();
+});
+document.getElementById("sign").addEventListener("click", function() {
+        sign();
+});
+document.getElementById("clear").addEventListener("click", function() {
+        clear();
+});
+
+//run all keyboard handlers
+document.addEventListener('keydown', function(event) {
+        if(buttonSafePattern.test(event.key) && event.key.charAt(0)!="F"){
+                numButton(event.key);
+        }
+        if(opSafePattern.test(event.key)){
+                opButton(event.key);
+        }
+        if(event.key == "Enter" || event.key == "=") {
+                equals();
+        }
+        if(event.key == ".") {
+                decimal();
+        }
+        if(event.key == "%") {
+                percent();
+        }
+        if(event.key == "s" || event.key == "S" || event.key =="_") {
+                sign();
+        }
+        if(event.key == "Backspace") {
+                clear();
+        }
+});
+
+
+
+//function that reverses a string
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+//securly execute eval function from discord
+function secureEval(expression) {
+        // Only allow numbers, operators, parentheses, and decimal points
+        
+        if (!safePattern.test(expression)) {
+                return "Eval Error";
+        }
+
+        if (expression.charAt(expression.length-1)=="0" && expression.charAt(expression.length-2)=="/") {
+               return "/0 Error";
+        }else {
+                try {
+                        // eslint-disable-next-line no-new-func
+                        return Function('"use strict"; return (' + expression + ')')();
+                } catch (e) {
+                        return "Eval Error";
+                        //throw new Error("Error evaluating expression.");
+                }
+        }
+}
